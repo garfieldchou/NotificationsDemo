d732d1c2338652f945ab5faa0b54536a7e6aa4d2
garfield_chou <garfield_chou@asus.com>
2017-03-06 16:58:52 +0800

    Initial commit

---
+++ app/src/main/java/com/garfield_chou/notificationsdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.garfield_chou.notificationsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
